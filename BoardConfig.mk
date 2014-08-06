CyanogenMod/android_device_samsung_d2lte

d2lte: Clean up Samsung device inheritance
* Separate apexqtmo and expressatt from d2lte
* Move common msm8960 bits to msm8960-common
* Do not dex to /cache
* Split proprietary lib handling between msm8960-common and device trees

Change-Id: Ibb82435955587e7016c28cf1272a43f92ed3fbe7

BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,90 +23,50 @@
 # inherit from common msm8960
 -include device/samsung/msm8960-common/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/d2lte/include
+# Assert
+TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
 
-# Kernel
-TARGET_KERNEL_SOURCE        := kernel/samsung/d2
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31 zcache
-BOARD_KERNEL_BASE           := 0x80200000
-BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01300000
-BOARD_KERNEL_PAGESIZE       := 2048
-TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
-
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
-# WiFi module
-WIFI_DRIVER_FW_PATH_P2P     :=
+# Kernel
+TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
 
-# Adreno configuration
-BOARD_EGL_CFG := device/samsung/d2lte/configs/egl.cfg
+# Audio
+BOARD_HAVE_AUDIENCE_A2220 := true
+BOARD_HAVE_DOCK_USBAUDIO := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
+BOARD_USES_FLUENCE_FOR_VOIP := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_VOIP := true
 
-# Recovery
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-TARGET_RECOVERY_FSTAB := device/samsung/d2lte/rootdir/etc/fstab.qcom
+# Bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2lte/bluetooth/vnd_d2.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2lte/bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
 
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Camera
+TARGET_ADD_ISO_MODE_1600 := true
+TARGET_NEED_CAMERA_ZSL := true
+TARGET_NEED_DISABLE_FACE_DETECTION := true
+TARGET_NEED_SAMSUNG_CAMERA_MODE := true
 
-# QCOM
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
+# Wifi
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_d2lte
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/d2lte/init/init_d2lte.c
 
-# bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2lte/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2lte/bluetooth/vnd_d2.txt
-
-# Disable initlogo, Samsungs framebuffer is weird
-TARGET_NO_INITLOGO := true
-
-# Use Audience A2220 chip
-BOARD_HAVE_AUDIENCE_A2220 := true
-
-# Use USB Dock Audio
-BOARD_HAVE_DOCK_USBAUDIO := true
-
-#camera abi compatiblily
-TARGET_DISPLAY_INSECURE_MM_HEAP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
-#TARGET_NEED_DISABLE_AUTOFOCUS := true
-ifeq ($(filter cm_apexqtmo cm_expressatt,$(TARGET_PRODUCT)),)
-  TARGET_NEED_CAMERA_ZSL := true
-  TARGET_ADD_ISO_MODE_1600 := true
-  TARGET_NEED_SAMSUNG_CAMERA_MODE := true
-else
-  TARGET_NEED_PREVIEW_SIZE_FIXUP := true
-endif
-TARGET_NEED_DISABLE_FACE_DETECTION := true
-
-# Needed for blobs
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
-# Audio
-BOARD_USES_SEPERATED_VOIP := true
-BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
-
-# We have the new GPS driver
-BOARD_HAVE_NEW_QC_GPS := true
-
-ifeq ($(filter cm_apexqtmo cm_expressatt cm_d2dcm,$(TARGET_PRODUCT)),)
-# Assert
-TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
-endif
-
 # Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/d2lte/releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/d2lte

Team-OSE-old/device_samsung_d2lte

BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,94 +23,52 @@
 # inherit from common msm8960
 -include device/samsung/msm8960-common/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/d2lte/include
+# Assert
+TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
 
-# Kernel
-TARGET_KERNEL_SOURCE        := kernel/samsung/d2
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31 zcache
-BOARD_KERNEL_BASE           := 0x80200000
-BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01500000
-BOARD_KERNEL_PAGESIZE       := 2048
-TARGET_KERNEL_CONFIG        := deathly_d2_defconfig
-TARGET_GCC_VERSION_ARM := 4.8-sm
-TARGET_GCC_VERSION_AND := 4.8-sm
-
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
-# WiFi module
-WIFI_DRIVER_FW_PATH_P2P     :=
+# Kernel
+TARGET_KERNEL_CONFIG := deathly_d2_defconfig
 
-# Adreno configuration
-BOARD_EGL_CFG := device/samsung/d2lte/configs/egl.cfg
+# Audio
+BOARD_HAVE_AUDIENCE_A2220 := true
+BOARD_HAVE_DOCK_USBAUDIO := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
+BOARD_USES_FLUENCE_FOR_VOIP := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_VOIP := true
 
-# Recovery
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-TARGET_RECOVERY_FSTAB := device/samsung/d2lte/rootdir/etc/fstab.qcom
+# Bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2lte/bluetooth/vnd_d2.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2lte/bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
 
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Camera
+TARGET_ADD_ISO_MODE_1600 := true
+TARGET_NEED_CAMERA_ZSL := true
+TARGET_NEED_SAMSUNG_CAMERA_MODE := true
 
-# QCOM
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
+# Wifi
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_d2lte
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/d2lte/init/init_d2lte.c
 
-# bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2lte/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2lte/bluetooth/vnd_d2.txt
-
-# Disable initlogo, Samsungs framebuffer is weird
-TARGET_NO_INITLOGO := true
-
-# Use Audience A2220 chip
-BOARD_HAVE_AUDIENCE_A2220 := true
-
-# Use USB Dock Audio
-BOARD_HAVE_DOCK_USBAUDIO := true
-
-#camera abi compatiblily
-TARGET_DISPLAY_INSECURE_MM_HEAP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
-#TARGET_NEED_DISABLE_AUTOFOCUS := true
-
-ifeq ($(filter liquid_apexqtmo liquid_expressatt,$(TARGET_PRODUCT)),)
-  TARGET_NEED_CAMERA_ZSL := true
-  TARGET_ADD_ISO_MODE_1600 := true
-  TARGET_NEED_SAMSUNG_CAMERA_MODE := true
-else
-  TARGET_NEED_PREVIEW_SIZE_FIXUP := true
-endif
-TARGET_NEED_DISABLE_FACE_DETECTION := true
-
-# Needed for blobs
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
-# Audio
-BOARD_USES_SEPERATED_VOIP := true
-BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
-
-ifeq ($(filter liquid_apexqtmo liquid_expressatt liquid_d2dcm,$(TARGET_PRODUCT)),)
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
-endif
-
 # External apps on SD
 TARGET_EXTERNAL_APPS = sdcard1
 
 # Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/d2lte/releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/d2lte